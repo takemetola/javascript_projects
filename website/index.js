6807ab061e69930837df81ba28f5f38dd4cc32c5
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-04-02 20:49:29 +0300

    .map() function

--- website/index.js
+++ website/index.js
@@ -523,3 +523,54 @@ function capitalize(element, index, array){
 }
 
 */
+
+//--------------------------------------------------------------------------
+
+// .map() = accepts a callback and applies that function to each element of an array, then returns a new array
+
+/*
+const numbers = [1, 2, 3, 4, 5];
+
+const squares = numbers.map(square);
+
+const cubes = numbers.map(cube);
+
+console.log(cubes);
+
+
+
+function square(element){
+    return Math.pow(element, 2);
+
+}
+
+function cube(element){
+    return Math.pow(element, 3);
+
+}
+
+*/
+
+/*
+const students = ["Bob", "Mark", "Sandy", "Kevin"];
+
+const studentsUpper = students.map(upperCase);
+
+console.log(studentsUpper);
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+*/
+
+const dates = ["2024-1-11", "1992-06-14", "1986-12-16"];
+
+const formattedDates = dates.map(formatDates);
+
+console.log(formattedDates);
+
+function formatDates(element) {
+    const parts = element.split("-");
+    return `${parts[2]}/${parts[1]}/${parts[0]}}`;
+
+}
