7ba26230af83c00fdc4b177276a832ff20bebe16
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-04-15 14:35:35 +0300

    sync/async

--- website/index.js
+++ website/index.js
@@ -1572,7 +1572,7 @@ function clearTimer(){
 
 //     <script type="module" src="index.js"></script> this line in index.html
 
-import {PI, getCircumference, getArea, getVolume} from './mathUtil.js';
+/* import {PI, getCircumference, getArea, getVolume} from './mathUtil.js';
 
 console.log(PI);
 
@@ -1586,6 +1586,31 @@ console.log(area);
 
 const volume = getVolume(10);
 
-console.log(volume);
+console.log(volume); */
 
 
+//--------------------------------------------------------------------------
+
+
+//synchronous = executes line by line consecutively in a sequential manner
+// code that waits for an operation to complete
+
+//asynchronous = allows multiple operations to be performed concurrently without waiting
+//                  doesn't block the execution flow and allows the program to continue
+// (1/0 operations, network requests, fetching data)
+//  handled with: callbacks, Promises, Async/Await
+
+
+function func1(callback){
+    setTimeout(() => {console.log("Task1"); callback()}, 3000); // setTimeout() is one of async calls
+
+}
+
+function func2(){
+    console.log("Task2")
+    console.log("Task3")
+    console.log("Task4")
+    
+}
+
+func1(func2);
