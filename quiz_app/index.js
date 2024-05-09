3d4020f13f9349c0f2a7a28b8f6a941d0dfd1e36
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-05-09 13:09:12 +0300

    quiz_app4

--- quiz_app/index.js
+++ quiz_app/index.js
@@ -77,15 +77,49 @@ function resetState(){
     }
 }
 
-function selectAnswer(){
+function selectAnswer(e){
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     }
     else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButton.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestions();
+    }
+    else{
+        showScore();
+    }
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        starQuiz();
+    }
+})
 
 starQuiz();
