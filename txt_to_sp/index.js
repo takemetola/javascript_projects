f46b1a7118e09816ec02c972bec264e41dd79331
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-05-13 19:44:52 +0300

    txt_sp3

--- txt_to_sp/index.js
+++ txt_to_sp/index.js
@@ -3,12 +3,18 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = () => {
+function populateVoiceList(){
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
 
     voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
+
+populateVoiceList();
+
+voiceSelect.addEventListener("change", ()=>{
+    speech.voice = voices[voiceSelect.value];
+})
 
 document.querySelector("button").addEventListener("click", () =>{
     speech.text = document.querySelector("textarea").value;
