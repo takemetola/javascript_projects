d84d6fa42494fc8e4d115c5cab4a88e7761bbf22
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-06-04 22:07:37 +0300

    initial commit

---
+++ animated_search/index.js
@@ -0,0 +1,9 @@
+let modelBox = document.getElementById("modelBox");
+
+document.onmouseout = function(){
+    modelBox.style.display = "block";
+}
+
+function closeModel(){
+    modelBox.style.display = "none";
+}
