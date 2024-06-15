92307e0ce9df4dd96feac720e34bdb7318b33abd
andriusjurkus <andrius.p.jurkus@gmail.com>
2024-06-15 12:41:52 +0300

    buttons functionality

--- flash_card2/index.js
+++ flash_card2/index.js
@@ -1,1 +1,34 @@
-console.log("hello there")
+const term = document.querySelector(".term");
+const definition = document.querySelector(".definition");
+const checkButton = document.querySelector(".check");
+const nextButton = document.querySelector(".next");
+
+let words = {
+    labas: "Hallo",
+    viso: "Tschüss",
+    medis: "Baum",
+    kinas: "Kino"
+}
+
+let data = Object.entries(words);
+
+checkButton.addEventListener("click", function(){
+    
+    definition.style.display = "block";
+    definition.innerHTML = `<h1>${randomTerm[1]}</h1>`;
+
+
+});
+
+nextButton.addEventListener("click", function(){
+    getRandomTerm();
+    definition.style.display = "none";
+})
+
+function getRandomTerm(){
+    randomTerm = data[Math.floor(Math.random() * data.length)];
+    term.innerHTML = `<h1>${randomTerm[0]}</h1>`;
+}
+
+
+
